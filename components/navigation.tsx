9269417d9ace426599edf7df7b40946b2839bbf7
ANJITH-B <anjithb200@gmail.com>
2025-12-11 18:05:59 +0530

    feat(ui): added retro theme on navbar

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,6 +1,5 @@
 
 "use client"
-
 import { useState } from "react"
 import { AnimatePresence, motion } from "framer-motion"
 import Image from "next/image"
@@ -15,19 +14,14 @@ const navItems = [
 ]
 
 const menuVariants = {
+
   hidden: {
     height: "3.5rem",
     borderRadius: "19px",
-    x: 0,
-    y: 0,
-
   },
   visible: {
     height: "18rem",
     borderRadius: "19px",
-    x: "0rem", // expands left
-    y: "0rem", // expands downward
-
   },
 };
 
@@ -37,7 +31,6 @@ const itemVariants = {
     opacity: 1,
     filter: "blur(0px)",
     scale: 1,
-
     transition: { delay: 0.2 + i * 0.1, duration: 0.3 },
   }),
 };
@@ -53,7 +46,6 @@ export function Navigation() {
 
   const scrollToSection = (id: string) => {
     setActiveItem(id)
-    setMenuOpen(false) // Close menu on mobile after selecting
     const element = document.getElementById(id)
     if (element) {
       element.scrollIntoView({ behavior: "smooth" })
@@ -61,19 +53,16 @@ export function Navigation() {
   }
 
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 p-4">
-      <div className="flex items-center justify-center    md:max-w-7xl mx-auto">
-        {/* Logo */}
-
-
-        {/* Navigation Pills */}
+    <motion.div initial={{ y: -100 }} animate={{ y: 0 }} className="fixed top-0 left-0 right-0 z-50 p-4" >
+      <div className="flex  items-center justify-center md:max-w-7xl mx-auto">
         <motion.nav
           variants={menuVariants}
           animate={menuOpen ? "visible" : "hidden"}
           transition={{ duration: 0.5, ease: "easeInOut" }}
-          className="w-full md:w-auto bg-[#D0D0D0]/30 backdrop-blur-3xl border-2 border-white rounded-full px-1.5 py-1 shadow-[0px_0px_30px_rgba(0,0,0,0.1)]  inset-shadow-[0_0_20px] inset-shadow-[#fff]">
-          <div className="flex justify-between  items-center gap-1 relative ">
+          className="w-full h-[3.5rem] md:w-auto bg-[#D0D0D0]/50 backdrop-blur-3xl border-2 border-black rounded-xl px-1.5 py-1 shadow-[0px_0px_30px_rgba(0,0,0,0.1)]  inset-shadow-[0_0_20px] inset-shadow-[#fff]">
 
+
+          <div className="flex justify-between  items-center gap-1 relative ">
             <div className="flex items-center gap-2 px-5 cursor-pointer" onClick={() => scrollToSection("home")}>
               <div className="w-10 h-10 rounded-full flex items-center justify-center flex-shrink-0">
                 <Image src="logos/logo.svg" alt="logo" width={32} height={32} />
@@ -81,19 +70,20 @@ export function Navigation() {
               <span className="font-bold text-lg">Digital Mitra</span>
             </div>
 
+
             {navItems.map((item) => (
               <button
                 key={item.id}
                 onClick={() => scrollToSection(item.id)}
                 className={`hidden md:block  relative px-6 py-3 text-sm font-medium transition-colors duration-200 z-10 ${activeItem === item.id
-                  ? "text-gray-900"
-                  : "text-gray-500 hover:text-gray-700"
+                  ? "text-gray-100"
+                  : "text-gray-500 hover:text-[#5C82A3] "
                   }`}
               >
                 {activeItem === item.id && (
                   <motion.div
                     layoutId="activeNav"
-                    className="absolute border-2 border-gray-100 shadow-[0px_0px_10px_rgba(0,0,0,0.1)]  inset-shadow-[0_0_20px] inset-shadow-gray-100 inset-0 bg-[#D0D0D0]/50 rounded-lg"
+                    className="absolute border-2 border-gray-800 shadow-[0px_0px_10px_rgba(0,0,0,0.1)]  inset-shadow-[0_0_20px] inset-shadow-black inset-0 bg-black/80 rounded-lg"
                     transition={{
                       type: "spring",
                       stiffness: 380,
@@ -128,7 +118,7 @@ export function Navigation() {
           </div>
         </motion.nav>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
